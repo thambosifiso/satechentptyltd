e7bdd41f6471a38c066d94d45f7ffee3907438d7
thambosifiso <ThamboS@bathinibona.co.za>
2026-01-07 19:53:26 +0200

    updated

---
+++ assets/js/router.js
@@ -0,0 +1,9 @@
+export function route(){
+  const h = location.hash || "#/";
+  const parts = h.replace("#/","").split("/");
+  return { raw:h, parts };
+}
+
+export function go(hash){
+  location.hash = hash;
+}
---
+++ assets/js/store.js
@@ -0,0 +1,31 @@
+const CART_KEY = "satech_cart_v4";
+
+export function getCart(){
+  try { return JSON.parse(localStorage.getItem(CART_KEY) || "{}"); }
+  catch { return {}; }
+}
+
+export function setCart(cart){
+  localStorage.setItem(CART_KEY, JSON.stringify(cart));
+}
+
+export function addToCart(id, qty=1){
+  const c = getCart();
+  c[id] = (c[id] || 0) + qty;
+  if(c[id] <= 0) delete c[id];
+  setCart(c);
+  return c;
+}
+
+export function setQty(id, qty){
+  const c = getCart();
+  if(qty <= 0) delete c[id];
+  else c[id] = qty;
+  setCart(c);
+  return c;
+}
+
+export function cartCount(){
+  const c = getCart();
+  return Object.values(c).reduce((a,b)=>a+b,0);
+}
