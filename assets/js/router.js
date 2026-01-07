79dc8fec20d21d09f338651a3b75452e81263586
thambosifiso <ThamboS@bathinibona.co.za>
2026-01-07 19:53:07 +0200

    up

--- assets/js/router.js
+++
@@ -1,7 +0,0 @@
-export function route(){
-  const h = location.hash || "#/";
-  const parts = h.replace("#/","").split("/");
-  return { raw:h, parts };
-}
-
-export function go(hash){ location.hash = hash; }
